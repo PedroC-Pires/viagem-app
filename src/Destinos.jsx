6c0d11f5846da23cd8d3e59eea195a9784bba168
PEDRO DA CONCEIÇÃO PIRES <pedro_c_pires@estudante.sesisenai.org.br>
2024-08-06 09:33:45 -0300

    Navegação de Componentes de Página

---
+++ src/Destinos.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Destino from './Destino';
+import FormDestino from './FormDestino';
+
+
+const Destinos = () => {
+    const [destinos, setDestinos] = useState([
+        { nome: 'Paris', descricao: 'A cidade do amor.' },
+        { nome: 'Nova York', descricao: 'A cidade que nunca dorme.' },
+        { nome: 'Tóquio', descricao: 'A capital do Japão.' }
+      ]);
+    
+      const adicionarDestino = (novoDestino) => {
+        setDestinos([...destinos, novoDestino]);
+      };
+    
+      return (
+        <div className="App">
+          <FormDestino adicionarDestino={adicionarDestino} />
+          {destinos.map((destino, index) => (
+            <Destino key={index} nome={destino.nome} descricao={destino.descricao} />
+          ))}
+        </div>
+      );
+};
+
+export default Destinos;
